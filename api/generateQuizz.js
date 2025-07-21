f1353395966e002c65778ceb41686b1374b40f6e
TshepoMelesi <twmelesi@gmail.com>
2025-07-21 16:27:55 +0200

    added a question generator

--- api/generateQuizz.js
+++ api/generateQuizz.js
@@ -1,3 +1,16 @@
-export default function handler(req, res){
-  res.status(200).json({message : "Hello World"})
+const random = (max = 1, min = 0) => {
+  return Math.round(Math.random() * (max - min) + min)
+}
+
+export default function generateQuizz(req, res){
+  const operators = ["+", "-", "*", "/"]
+  const terms = []
+  const termsTotal = random(5)
+  
+  for(let i = 0; i < termsTotal; i++){
+    terms.push(random(20, 2))
+
+    if(i < termsTotal) terms.push(operators[random(operators.length-1)])
+  }
+  res.status(200).json({question : terms})
 }
